dd8239177743aef118df8aacb01038e3bf7d06e8
Haydn Martin <haydnmartin6@gmail.com>
2023-11-09 11:16:01 +0000

    Testing raw data generation script

--- bank-marketing-e2e-model/src/data/raw_data_gen.py
+++ bank-marketing-e2e-model/src/data/raw_data_gen.py
@@ -1,6 +1,7 @@
 from ucimlrepo import fetch_ucirepo
 from ucimlrepo.dotdict import dotdict
 from pandas import DataFrame
+import os
 
 
 class FetchUCIData:
@@ -16,8 +17,11 @@ class FetchUCIData:
         format_data: Returns a pandas dataframe given raw UCI dataset 
     '''
 
-    def fetch_raw_data(self) -> dotdict.dotdict:
-        return fetch_ucirepo(id=self.uci_repo)
+    def fetch_raw_data(self) -> dotdict:
+        try:
+            return fetch_ucirepo(id=self.uci_repo)
+        except Exception as e:
+            print(f"Error fetching raw data: {e}")
 
     def format_data(self) -> DataFrame:
         # Get raw data
@@ -28,3 +32,28 @@ class FetchUCIData:
         # Combine to one df
         x_vals['target'] = y_vals
         return x_vals
+
+
+class SaveRawTrainingData:
+    def __init__(self, raw_training_df, raw_data_store_path):
+        self.raw_training_df = raw_training_df
+        self.raw_data_store_path = raw_data_store_path
+    '''
+    A class to save a pandas dataframe in a specified directory.
+    
+    Methods:
+        save_raw_training_data: Saves a training data pandas df in a specified raw 
+                                training data directory 
+    '''
+
+    def save_raw_training_data(self):
+        # File path to store data
+        try:
+            # File path to store data
+            file_path = os.path.join(self.raw_data_store_path,
+                                     'raw_training_data.csv')
+            # Save pandas df as csv to this dir
+            self.raw_training_df.to_csv(file_path,
+                                        index=False)
+        except Exception as e:
+            print(f"Error saving file: {e}")
