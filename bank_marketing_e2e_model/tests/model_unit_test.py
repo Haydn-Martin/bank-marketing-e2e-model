73c0bf7912a9db2e6e1907ee4abb4440ea40ba1f
Haydn Martin <haydnmartin6@gmail.com>
2023-11-24 14:11:58 +0000

    Adding web page unit test

--- bank_marketing_e2e_model/tests/model_unit_test.py
+++ bank_marketing_e2e_model/tests/model_unit_test.py
@@ -1,12 +1,16 @@
 import unittest
+
 import pandas as pd
 import joblib
 import yaml
 import os
 
 
-class TestModel(unittest.TestCase):
-    def load_model_pipeline(self):
+class ModelUnitTest(unittest.TestCase):
+
+    # Static method for loading config
+    @staticmethod
+    def load_model_pipeline():
         # Load config
         parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  # Get parent dir
         with open(os.path.join(parent_dir, 'config.yaml'), 'r') as config_file:
@@ -14,6 +18,7 @@ class TestModel(unittest.TestCase):
         # Load model pipeline using config
         return joblib.load(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', config['output']['model_path'])))
 
+    # Perform unit test
     def test_inference(self):
         # Create an inference dictionary that should give us a result of 1
         inference_df = pd.DataFrame(
@@ -45,7 +50,7 @@ class TestModel(unittest.TestCase):
         # Define the expected output
         expected_output = 1
         # Assert that the result matches the expected output
-        self.assertEqual(result, expected_output, "Model inference result does not match expected output")
+        self.assertEqual(result, expected_output, "Model inference result does not match expected output.")
 
 
 if __name__ == '__main__':
---
+++ bank_marketing_e2e_model/tests/web_page_unit_test.py
@@ -0,0 +1,52 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+
+class WebPageDropUnitTest(unittest.TestCase):
+
+    # Set up
+    def setUp(self):
+        # Set driver
+        self.driver = webdriver.Chrome()
+        # Set local url
+        self.local_url = 'http://localhost:8501'
+
+    # Perform unit test
+    def test_web_page(self):
+        # Start the web driver
+        driver = self.driver
+        # Open the Streamlit app
+        driver.get(self.local_url)
+        time.sleep(10)  # zzzzzzzzzz
+        # Select first drop-down box
+        driver.find_element(
+            By.CSS_SELECTOR,
+            '[data-testid="stSelectbox"]'
+        ).click()
+        time.sleep(3)  # zzz
+        # Select an option from drop-down
+        driver.find_element(
+            By.XPATH,
+            '/html/body/div/div[2]/div/div/div/div/div/div/ul/div/div/li[2]/div/div/div'
+        ).click()
+        time.sleep(3)  # zzz
+        # Get result
+        result = driver.find_element(
+            By.XPATH,
+            '/html/body/div/div[1]/div[1]/div/div/div/section/div[1]/div/div/div[14]/div/div/div/div[1]/div[1]'
+        ).text
+        # Define the expected output
+        expected_output = 'management'
+        self.assertEqual(result, expected_output, "Selected item does not match expected output.")
+
+    # Close web page
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
